76a4e35c3f5cfafa00f1079254ad4eddf6b682eb
AASSEM Youssef <aassemyoussef@AASSEMs-MacBook-Pro.local>
2019-01-04 11:13:11 +0100

    adding Firebase authentification for registeration

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -29,7 +29,14 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (AuthDataResult, Error) in
+            guard let user = AuthDataResult?.user else {
+                print(Error.debugDescription)
+                return
+            }
+            print("Success registering user \(user.email!) !")
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+        }
         
         //TODO: Set up a new user on our Firbase database
         
